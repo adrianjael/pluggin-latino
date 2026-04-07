f92f11f4edabad0ef1f06bba36d15d7cde640a11
adrianjael <adrianjael15@gmail.com>
2026-04-07 16:31:53 -0500

    cleanup: remove pelispedia and prioritize embed69 with JWT fix

--- providers/embed69.js
+++ providers/embed69.js
@@ -1,6 +1,6 @@
 /**
  * embed69 - Built from src/embed69/
- * Generated: 2026-04-07T21:31:10.160Z
+ * Generated: 2026-04-07T21:31:53.324Z
  */
 var __create = Object.create;
 var __defProp = Object.defineProperty;
