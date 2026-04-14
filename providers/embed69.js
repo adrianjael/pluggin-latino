7f838fef4e1a70eea003e941861923e68f86c420
adrianjael <adrianjael15@gmail.com>
2026-04-14 10:37:03 -0500

    Fix: Filtrar links /d/ redundantes v7.2.5

--- providers/embed69.js
+++ providers/embed69.js
@@ -1,6 +1,6 @@
 /**
  * embed69 - Built from src/embed69/
- * Generated: 2026-04-14T15:35:28.931Z
+ * Generated: 2026-04-14T15:37:03.092Z
  */
 var __create = Object.create;
 var __defProp = Object.defineProperty;
@@ -1925,6 +1925,8 @@ function getStreams(tmdbId, mediaType, season, episode) {
               }
             }
             if (seenUrls.has(decodedLink))
+              continue;
+            if (decodedLink.includes("embed69.org/d/"))
               continue;
             seenUrls.add(decodedLink);
             const sLabel = embed.servername || "Servidor";
