15d33d712a5efd6db07d985439601aa2ff0b8a90
adrianjael <adrianjael15@gmail.com>
2026-04-14 12:16:13 -0500

    Fix: Distinción precisa entre Latino y Español (España) v7.4.4

--- providers/embed69.js
+++ providers/embed69.js
@@ -1,6 +1,6 @@
 /**
  * embed69 - Built from src/embed69/
- * Generated: 2026-04-14T17:15:05.146Z
+ * Generated: 2026-04-14T17:16:13.363Z
  */
 var __create = Object.create;
 var __defProp = Object.defineProperty;
@@ -1923,7 +1923,7 @@ function getStreams(tmdbId, mediaType, season, episode, title, year) {
       const seenUrls = /* @__PURE__ */ new Set();
       const langMap = {
         "LAT": "Latino",
-        "ESP": "Latino",
+        "ESP": "Espa\xF1ol",
         "SUB": "Subtitulado"
       };
       data.forEach((item) => {
