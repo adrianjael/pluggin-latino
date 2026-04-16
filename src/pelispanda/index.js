2879d922b6fb5d20be8339732fdb8b579156e747
adrianjael <adrianjael15@gmail.com>
2026-04-16 10:05:36 -0500

    chore: remove src folder from repository and restore original gitignore

--- src/pelispanda/index.js
+++
@@ -1,8 +0,0 @@
-const { extractStreams } = require('./extractor.js');
-const { finalizeStreams } = require('../utils/engine.js');
-
-async function getStreams(tmdbId, mediaType, season, episode, title, year) {
-    const streams = await extractStreams(tmdbId, mediaType, season, episode, title, year);
-    return await finalizeStreams(streams, 'PelisPanda', title);
-}
-module.exports = { getStreams };
